05315354385b537f6e465f3e664238d4e12c04b9
jsonlu <1144136154@qq.com>
2017-04-30 11:04:17 +0800

    Site updated: 2017-04-30 11:04:17

--- js/google-code-prettify/lang-pascal.js
+++ js/google-code-prettify/lang-pascal.js
@@ -1,3 +1,3 @@
-// build time:Sun Apr 30 2017 10:11:25 GMT+0800 (CST)
+// build time:Sun Apr 30 2017 11:04:13 GMT+0800 (CST)
 PR.registerLangHandler(PR.createSimpleLexer([["str",/^(?:\'(?:[^\\\'\r\n]|\\.)*(?:\'|$))/,null,"'"],["pln",/^\s+/,null," \r\n\t "]],[["com",/^\(\*[\s\S]*?(?:\*\)|$)|^\{[\s\S]*?(?:\}|$)/,null],["kwd",/^(?:ABSOLUTE|AND|ARRAY|ASM|ASSEMBLER|BEGIN|CASE|CONST|CONSTRUCTOR|DESTRUCTOR|DIV|DO|DOWNTO|ELSE|END|EXTERNAL|FOR|FORWARD|FUNCTION|GOTO|IF|IMPLEMENTATION|IN|INLINE|INTERFACE|INTERRUPT|LABEL|MOD|NOT|OBJECT|OF|OR|PACKED|PROCEDURE|PROGRAM|RECORD|REPEAT|SET|SHL|SHR|THEN|TO|TYPE|UNIT|UNTIL|USES|VAR|VIRTUAL|WHILE|WITH|XOR)\b/i,null],["lit",/^(?:true|false|self|nil)/i,null],["pln",/^[a-z][a-z0-9]*/i,null],["lit",/^(?:\$[a-f0-9]+|(?:\d+(?:\.\d*)?|\.\d+)(?:e[+\-]?\d+)?)/i,null,"0123456789"],["pun",/^.[^\s\w\.$@\'\/]*/,null]]),["pascal"]);
 //rebuild by neat 
--- js/google-code-prettify/lang-proto.js
+++ js/google-code-prettify/lang-proto.js
@@ -1,3 +1,3 @@
-// build time:Sun Apr 30 2017 10:11:25 GMT+0800 (CST)
+// build time:Sun Apr 30 2017 11:04:13 GMT+0800 (CST)
 PR.registerLangHandler(PR.sourceDecorator({keywords:"bytes,default,double,enum,extend,extensions,false,group,import,max,message,option,optional,package,repeated,required,returns,rpc,service,syntax,to,true",types:/^(bool|(double|s?fixed|[su]?int)(32|64)|float|string)\b/,cStyleComments:!0}),["proto"]);
 //rebuild by neat 
