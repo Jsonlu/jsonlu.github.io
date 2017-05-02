bec82d362172b4cd281ba16ad6973ec9ab50a03a
jsonlu <1144136154@qq.com>
2017-05-02 16:28:46 +0800

    Site updated: 2017-05-02 16:28:46

--- js/google-code-prettify/lang-rd.js
+++ js/google-code-prettify/lang-rd.js
@@ -1,3 +1,3 @@
-// build time:Tue May 02 2017 16:08:21 GMT+0800 (CST)
+// build time:Tue May 02 2017 16:28:36 GMT+0800 (CST)
 PR.registerLangHandler(PR.createSimpleLexer([["pln",/^[\t\n\r \xA0]+/,null,"\t\n\r  "],["com",/^%[^\r\n]*/,null,"%"]],[["lit",/^\\(?:cr|l?dots|R|tab)\b/],["kwd",/^\\[a-zA-Z@]+/],["kwd",/^#(?:ifn?def|endif)/],["pln",/^\\[{}]/],["pun",/^[{}()\[\]]+/]]),["Rd","rd"]);
 //rebuild by neat 
