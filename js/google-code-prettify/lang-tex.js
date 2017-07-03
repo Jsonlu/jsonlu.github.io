bf4d258a806a9c97471142bac052c04200d1f9b5
jsonlu <1144136154@qq.com>
2017-07-03 23:37:44 +0800

    Site updated: 2017-07-03 23:37:44

--- js/google-code-prettify/lang-tex.js
+++ js/google-code-prettify/lang-tex.js
@@ -1,3 +1,3 @@
-// build time:Mon Jul 03 2017 23:36:11 GMT+0800 (CST)
+// build time:Mon Jul 03 2017 23:37:34 GMT+0800 (CST)
 PR.registerLangHandler(PR.createSimpleLexer([["pln",/^[\t\n\r \xA0]+/,null,"\t\n\r  "],["com",/^%[^\r\n]*/,null,"%"]],[["kwd",/^\\[a-zA-Z@]+/],["kwd",/^\\./],["typ",/^[$&]/],["lit",/[+-]?(?:\.\d+|\d+(?:\.\d*)?)(cm|em|ex|in|pc|pt|bp|mm)/i],["pun",/^[{}()\[\]=]+/]]),["latex","tex"]);
 //rebuild by neat 
