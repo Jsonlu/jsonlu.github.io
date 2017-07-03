3a570d76002df847ac8617f86767df43f8fc30e3
jsonlu <1144136154@qq.com>
2017-07-03 23:36:17 +0800

    Site updated: 2017-07-03 23:36:17

--- js/google-code-prettify/lang-go.js
+++ js/google-code-prettify/lang-go.js
@@ -1,3 +1,3 @@
-// build time:Mon Jul 03 2017 23:34:08 GMT+0800 (CST)
+// build time:Mon Jul 03 2017 23:36:11 GMT+0800 (CST)
 PR.registerLangHandler(PR.createSimpleLexer([["pln",/^[\t\n\r \xA0]+/,null,"\t\n\r  "],["pln",/^(?:\"(?:[^\"\\]|\\[\s\S])*(?:\"|$)|\'(?:[^\'\\]|\\[\s\S])+(?:\'|$)|`[^`]*(?:`|$))/,null,"\"'"]],[["com",/^(?:\/\/[^\r\n]*|\/\*[\s\S]*?\*\/)/],["pln",/^(?:[^\/\"\'`]|\/(?![\/\*]))+/i]]),["go"]);
 //rebuild by neat 
--- js/google-code-prettify/lang-proto.js
+++ js/google-code-prettify/lang-proto.js
@@ -1,3 +1,3 @@
-// build time:Mon Jul 03 2017 23:34:08 GMT+0800 (CST)
+// build time:Mon Jul 03 2017 23:36:11 GMT+0800 (CST)
 PR.registerLangHandler(PR.sourceDecorator({keywords:"bytes,default,double,enum,extend,extensions,false,group,import,max,message,option,optional,package,repeated,required,returns,rpc,service,syntax,to,true",types:/^(bool|(double|s?fixed|[su]?int)(32|64)|float|string)\b/,cStyleComments:!0}),["proto"]);
 //rebuild by neat 
