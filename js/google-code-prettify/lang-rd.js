0ea8cbb0f1bc7210d4db27ed47b37af001a837c9
jsonlu <jsonlu@qq.com>
2017-09-03 15:06:45 +0800

    Site updated: 2017-09-03 15:06:45

--- js/google-code-prettify/lang-rd.js
+++
@@ -1,3 +0,0 @@
-// build time:Mon Jul 03 2017 23:41:10 GMT+0800 (CST)
-PR.registerLangHandler(PR.createSimpleLexer([["pln",/^[\t\n\r \xA0]+/,null,"\t\n\r  "],["com",/^%[^\r\n]*/,null,"%"]],[["lit",/^\\(?:cr|l?dots|R|tab)\b/],["kwd",/^\\[a-zA-Z@]+/],["kwd",/^#(?:ifn?def|endif)/],["pln",/^\\[{}]/],["pun",/^[{}()\[\]]+/]]),["Rd","rd"]);
-//rebuild by neat 
--- js/google-code-prettify/lang-yml.js
+++
@@ -1,3 +0,0 @@
-// build time:Mon Jul 03 2017 23:41:10 GMT+0800 (CST)
-PR.registerLangHandler(PR.createSimpleLexer([["pun",/^[:|>?]+/,null,":|>?"],["dec",/^%(?:YAML|TAG)[^#\r\n]+/,null,"%"],["typ",/^[&]\S+/,null,"&"],["typ",/^!\S*/,null,"!"],["str",/^"(?:[^\\"]|\\.)*(?:"|$)/,null,'"'],["str",/^'(?:[^']|'')*(?:'|$)/,null,"'"],["com",/^#[^\r\n]*/,null,"#"],["pln",/^\s+/,null," \t\r\n"]],[["dec",/^(?:---|\.\.\.)(?:[\r\n]|$)/],["pun",/^-/],["kwd",/^[\w-]+:[ \r\n]/],["pln",/^\w+/]]),["yaml","yml"]);
-//rebuild by neat 
